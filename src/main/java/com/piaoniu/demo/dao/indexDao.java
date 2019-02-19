349c87bcff3453b3b02223489da85d82c21c47b0
pgj123 <940998084@qq.com>
2019-02-19 09:24:59 +0800

    controller

--- src/main/java/com/piaoniu/demo/dao/indexDao.java
+++ src/main/java/com/piaoniu/demo/dao/indexDao.java
@@ -1,4 +1,10 @@
 package com.piaoniu.demo.dao;
 
+import com.piaoniu.demo.pojo.ShowType;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
 public interface indexDao {
+    List<ShowType> findShowtype();
 }
--- src/main/java/com/piaoniu/demo/service/IndexService.java
+++ src/main/java/com/piaoniu/demo/service/IndexService.java
@@ -1,4 +1,9 @@
 package com.piaoniu.demo.service;
 
+import com.piaoniu.demo.pojo.ShowType;
+
+import java.util.List;
+
 public interface IndexService {
+    List<ShowType> findShowtype();
 }
